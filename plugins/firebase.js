f2eb4cca37469fb9df161a817c4bcabb64760a5f
Pascal <pascal@luther.ch>
2020-03-12 08:04:08 +0100

    added nuxt-fire-demo config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,16 +3,17 @@ import 'firebase/firestore'
 
 if (!firebase.apps.length) {
   const config = {
-    apiKey: '<replace this>',
-    authDomain: '<replace this>',
-    databaseURL: '<replace this>',
-    projectId: '<replace this>',
-    storageBucket: '<replace this>',
-    messagingSenderId: '<replace this>',
-    appId: '<replace this>',
-    measurementId: '<replace this>',
-    fcmPublicVapidKey: '<replace this>'
-  }
+    apiKey: 'AIzaSyDa-YwgWTp2GDyVYEfv-XLb62100_HoEvU',
+    authDomain: 'nuxt-fire-demo.firebaseapp.com',
+    databaseURL: 'https://nuxt-fire-demo.firebaseio.com',
+    projectId: 'nuxt-fire-demo',
+    storageBucket: 'nuxt-fire-demo.appspot.com',
+    messagingSenderId: '807370470428',
+    appId: '1:807370470428:web:26da98c86c3fd352',
+    measurementId: 'G-XT6PVC1D4X',
+    fcmPublicVapidKey:
+      'BL_xoiuOe5vbb2vJkCNnuswn03NwCsyCkJUgRbuQA5tpg7J4E4z50MO8b-wrrad6fcysYAaFjHqU7D9o0oCWL8w'
+    }
   firebase.initializeApp(config)
   firebase.firestore()
 }
